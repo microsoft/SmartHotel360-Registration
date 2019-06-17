4df572aa47b2b35d261df2e57afbd415f4cae6f5
jmanuelcorral <jcorral@plainconcepts.com>
2019-06-17 22:00:27 +0200

    Tutorial and Deploy script

--- Deploy/k8s/deploy.ps1
+++ Deploy/k8s/deploy.ps1
@@ -1,6 +1,7 @@
 Param(
         [parameter(Mandatory=$false)][string]$dnsname="smhotel360win" 
 )
+
 Write-Host "Resolving DNS to Gateway public IP" -ForegroundColor Green
 $ipaddress = $(kubectl get service smgateway-nginx-ingress-controller -n kube-system)[1] | %{ $_.Split('   ')[9];}
 Write-Host $ipaddress
@@ -11,3 +12,9 @@ Write-Host $resid
 $jsonresponse = az network public-ip update --ids $resid --dns-name $dnsname
 $externalDns = ($jsonresponse | ConvertFrom-Json).dnsSettings.fqdn
 Write-Host "$externalDns is pointing to Cluster public ip $ipaddress"
+Read-Host -Prompt "Press Enter to continue"
+# Gateway Generation
+.\token-replace.ps1 -tokens @{ApplicationHost=$externalDns} -inputFile .\kustomize\ingress\deployment.template -outputFile .\kustomize\ingress\deployment.yaml
+Read-Host -Prompt "Press Enter to continue"
+Write-Host "Deploying application"
+kubectl apply -k .\kustomize
